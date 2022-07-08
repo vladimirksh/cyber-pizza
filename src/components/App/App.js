11a023cbd13aa7bacd28ea282035bffa9c6079d0
Vladimir Kashankov <74548402+vladimirksh@users.noreply.github.com>
2022-07-08 19:32:30 +0300

    add skeleton

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,23 @@
+import React, { useState, useEffect } from "react";
 import Header from "../Header/Header";
 import Categories from "../Categories/Categories";
 import Sort from "../Sort/Sort";
 import Pizza from "../Pizza/Pizza";
-
-import pizzas from "../../assets/pizzas.json";
+import PizzaSkeleton from "../PizzaSkeleton/PizzaSkeleton";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://62c828458c90491c2cb00d05.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -17,8 +29,8 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((pizza) => (
-              <Pizza {...pizza} key={pizza.id} />
+            {items.map((item) => (
+              <Pizza {...item} key={item.id} />
             ))}
           </div>
         </div>
---
+++ src/components/PizzaSkeleton/PizzaSkeleton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const PizzaSkeleton = (props) => (
+  <ContentLoader
+    speed={2}
+    width={280}
+    height={491}
+    viewBox="0 0 280 465"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <circle cx="135" cy="135" r="125" />
+    <rect x="0" y="279" rx="10" ry="10" width="280" height="23" />
+    <rect x="0" y="315" rx="10" ry="10" width="280" height="88" />
+    <rect x="0" y="415" rx="10" ry="10" width="95" height="30" />
+    <rect x="125" y="410" rx="24" ry="24" width="152" height="45" />
+  </ContentLoader>
+);
+
+export default PizzaSkeleton;
